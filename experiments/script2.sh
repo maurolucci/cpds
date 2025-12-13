f5606ad387658aa8587bfb94ca812c0df16c2f4c
Mauro Lucci <maurolucci.14@gmail.com>
2025-12-12 21:01:03 -0300

    New initialization for model EFPS

--- experiments/script2.sh
+++ experiments/script2.sh
@@ -33,13 +33,13 @@ do
             # time $BIN -s $solver -w $omega -f $INPUT$name -n $NRUN -t $TIMEOUT -o $OUTPUT --out-prop --init-efps
              #time $BIN -s $solver -w $omega -f $INPUT$name -n $NRUN -t $TIMEOUT -o $OUTPUT --in-prop --out-prop --init-efps
             time $BIN -s $solver -w $omega -f $INPUT$name -n $NRUN -t $TIMEOUT -o $OUTPUT --out-prop --init-efps-1
-            for cutmax in "${CUTMAX[@]}"
-            do
-                for cutfreq in "${CUTFREQ[@]}"
-                do
+            # for cutmax in "${CUTMAX[@]}"
+            # do
+                # for cutfreq in "${CUTFREQ[@]}"
+                # do
                     #time $BIN -s $solver -w $omega -f $INPUT$name -n $NRUN -t $TIMEOUT -o $OUTPUT --out-prop --init-efps --cuts --cut-max $cutmax --cut-freq $cutfreq
-                done
-            done
+                # done
+            # done
         done
     done
 done
